2302722c01a4842ca3748d1ff968a84268848481
Savin Angel-Mario <savin@angelmario.eu>
2023-08-15 15:28:40 +0300

    Started setting up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), image(), sitemap(), mdx()],
+  output: "server",
+  adapter: cloudflare()
+});

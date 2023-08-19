4fdbca668f8af6d9fb97346c45ac2a1a07b71d2b
Savin Angel-Mario <savin@angelmario.eu>
2023-08-20 02:47:05 +0300

    Test build

---
+++ .vercel/output/static/_astro/hoisted.f9d25507.js
@@ -0,0 +1,1 @@
+import{i}from"./client.c7ae6be5.js";i();
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,5 +17,5 @@ export default defineConfig({
     assets: true
   },
   output: "server",
-  adapter: vercel()
+  adapter: cloudflare(),
 });

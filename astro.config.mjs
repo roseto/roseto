3ebcf9f8ed98ee59416c793da5c5fab7fe6c8321
Savin Angel-Mario <savin@angelmario.eu>
2023-08-20 03:56:17 +0300

    Switched to vercel edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercelEdge from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +14,5 @@ export default defineConfig({
     applyBaseStyles: false
   }), sitemap(), mdx(), prefetch(), image()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercelEdge(),
 });

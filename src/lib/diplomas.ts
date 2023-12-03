e2665c813d76f1207c08fe0f58cdbede701c139c
Savin Angel-Mario <savin@angelmario.eu>
2023-12-03 14:04:54 +0200

    Updated project fetching

--- src/lib/diplomas.ts
+++ src/lib/diplomas.ts
@@ -2,11 +2,13 @@ import type { Diploma } from "@/types/Diploma";
 import { sanityClient } from "./sanity";
 
 export const getDiploma = async (id: string) => {
+	// Diploma has a reference to "project" 
+	// Project has a field called "id"
+	// Add a custom field to the query called "projectId" that is the id of the project
 	const diploma = await sanityClient.fetch<Diploma | null>(`*[_type == "diploma" && id == $id][0] {
 		id,
 		name,
-		blogPost,
-		projectId
+		"projectId": project->id,
 	}`, { id: id.toUpperCase() });
 
 	return diploma;

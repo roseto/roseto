573af86e9f7ada6e454ba5e5dd2eaf4166b24cd4
Savin Angel-Mario <savin@angelmario.eu>
2024-06-10 06:55:55 +0000

    chore: fixed builds and added cloudflare analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,34 +4,36 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	server: {
-		port: 3000
-	},
-	site: "https://roseto.dev",
-	trailingSlash: "ignore",
-	integrations: [
-		react(), 
-		tailwind({
-			applyBaseStyles: false
-		}), 
-		sitemap(), 
-		mdx(), 
-		prefetch()
-	],
-	build: {
-		format: "file"
-	},
-	output: "hybrid",
-	adapter: cloudflare({
-		imageService: "compile",
-		runtime: {
-			mode: "local",
-			persistTo: ".wrangler/state/v3"
-		},
-	})
+  server: {
+    port: 3000
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ["fs/promises"],
+      },  
+    }
+  },
+  site: "https://roseto.dev",
+  trailingSlash: "ignore",
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), mdx(), prefetch(), partytown()],
+  build: {
+    format: "file"
+  },
+  output: "hybrid",
+  adapter: cloudflare({
+    imageService: "compile",
+    runtime: {
+      mode: "local",
+      persistTo: ".wrangler/state/v3"
+    }
+  })
 });
